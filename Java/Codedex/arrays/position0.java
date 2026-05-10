146efcc3c4c61c9bcc82db1f93d04e3d562118f1
Abdullahi Omer <abdullahi.omer17@gmail.com>
2026-05-09 22:05:20 -0400

    "Learning Arrays"

---
+++ Java/Codedex/arrays/position0.java
@@ -0,0 +1,16 @@
+/* Create a string array called sodaDrinks and add all of your favorite carbonated drinks ranked, starting with your favorite one at index 0.
+
+Attempt to access the third, fourth, and sixth item of your array.
+
+What's your output? 🥤 */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        String[] sodaDrinks = {"Crush", "C+", "Dr. Pepper", "Coke", "Pepsi", "Mountain Dew", "Diet Coke", "Blah"};
+
+        System.out.println(sodaDrinks[2]);
+        System.out.println(sodaDrinks[3]);
+        System.out.println(sodaDrinks[5]);
+    }
+}
